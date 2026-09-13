a9bde51f368b67e67fb07fb1cae64754d29d091c
viniciusmbs <viniciusmbs@gmail.com>
2026-09-13 19:22:40 -0300

    Fix typo in Canal Brasil entry

--- src/data/playlist.ts
+++ src/data/playlist.ts
@@ -157,7 +157,7 @@ https://alerquina54105.embedtv.lat/amc
 #EXTINF:-1 group-title="FILMES & SÉRIES",AXN
 https://alerquina54105.embedtv.lat/axn
 
-#EXTINF:-1 group-title="FILMES & SÉRIES",Canal Brasil
+#EXTINF:-1 group-title="FILMES & SÉRIES",Canal Brasi
 https://rdcanais.net/canalbrasil
 
 #EXTINF:-1 group-title="FILMES & SÉRIES",Cinemax
