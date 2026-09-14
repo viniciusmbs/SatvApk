4316ed6ca1e4f608e34142f5b1a0b2547b6adffb
viniciusmbs <viniciusmbs@gmail.com>
2026-09-14 08:47:11 -0300

    Update playlist.ts

--- src/data/playlist.ts
+++ src/data/playlist.ts
@@ -5,6 +5,7 @@ http://tjtor8411.com:80/Osiel123/Felicidade321/71766.ts
 
 #EXTINF:-1 group-title="DOCUMENTÁRIOS",Animal Planet
 https://rdcanais.net/animalplanet
+
 #EXTINF:-1 group-title="DOCUMENTÁRIOS",Arte 1
 https://rdcanais.net/arte1
 
